6c0ad938e065273ed0f7eecf25e4c20599926c1f
jose <jose@10.10.0.60>
2015-06-28 10:41:30 -0500

    Modificación de Aplicacion.java

--- src/main/java/com/tecsup/spring/Aplicacion.java
+++ src/main/java/com/tecsup/spring/Aplicacion.java
@@ -4,6 +4,13 @@ public class Aplicacion {
     
     public static void main(String[] args) {
         System.out.println("Tecsup");
+        
+        Aplicacion.mensaje();
     }
-     
+    
+   
+    public static void mensaje() {
+        System.out.println("Hola Mundo");
+    }
+   
 }
